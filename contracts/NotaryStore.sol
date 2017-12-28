fa21f261bee0c8d17c7f076c915f952da9ebf49e
Dan DeGreef <daniel_degreef@condenast.com>
2017-12-28 15:56:30 -0600

    update to use logs and events instead of state

--- contracts/NotaryStore.sol
+++ contracts/NotaryStore.sol
@@ -1,57 +1,17 @@
 pragma solidity ^0.4.18;
 
 contract NotaryStore {
-  Entry[] private store;
 
-  struct Entry {
-    address signer;
-    string documentHash;
-  }
-
-  event FoundEntry(
+  event Entry(
     address signer,
     string documentHash
   );
 
   /**
-    Returns index of new Entry.
+    create a new Entry in the logs.
    */
-  function create(string documentHash) public returns (uint) {
-    return store.push(Entry(msg.sender, documentHash)) - 1;
-  }
-
-  function read(uint index) public {
-    require(index < store.length);
-    FoundEntry(store[index].signer, store[index].documentHash);
-  }
-
-  /**
-    Finds all the notary signers for a particular hash.
-    Creates an event for each of the signers of a hash.
-   */
-  function findAll(string documentHash) public returns (uint) {
-    uint entriesFound = 0;
-    for (uint i = 0; i<store.length; i++) {
-      if (keccak256(store[i].documentHash) == keccak256(documentHash)) {
-        FoundEntry(store[i].signer, store[i].documentHash);
-        entriesFound++;
-      }
-    }
-    return entriesFound;
-  }
-
-  /**
-    Finds all the notary documentHashes for a particular address.
-    Creates an event for each of the hashes this address has signed.
-   */
-  function findAll(address sender) public returns (uint) {
-    uint entriesFound = 0;
-    for (uint i = 0; i<store.length; i++) {
-      if (store[i].signer == sender) {
-        FoundEntry(store[i].signer, store[i].documentHash);
-        entriesFound++;
-      }
-    }
-    return entriesFound;
+  function create(string documentHash) public returns (bool) {
+    Entry(msg.sender, documentHash);
+    return true;
   }
 }
--- test/TestNotaryStore.sol
+++ test/TestNotaryStore.sol
@@ -8,39 +8,11 @@ contract TestNotaryStore {
   NotaryStore notaryStore = NotaryStore(DeployedAddresses.NotaryStore());
 
   string foobarHash = "0a50261ebd1a390fed2bf326f2673c145582a6342d523204973d0219337f81616a8069b012587cf5635f6925f1b56c360230c19b273500ee013e030601bf2425";
-  string anotherHash = "1a50261ebd1a390fed2bf326f2673c145582a6342d523204973d0219337f81616a8069b012587cf5635f6925f1b56c360230c19b273500ee013e030601bf2425";
 
   // Testing the adopt() function
   function testUserCanWrite() public {
-    uint index = notaryStore.create(foobarHash);
-    Assert.equal(index, 0, "should write and return the array position");
-    uint index2 = notaryStore.create(anotherHash);
-    Assert.equal(index2, 1, "should write and return the array position");
-  }
-
-  // function testRead() public {
-  //   // TODO: add test to listen for event
-  //   notaryStore.read(0);
-  //   notaryStore.read(123); // TODO: figure out how to test for errors.
-  // }
-
-  function testFindAllBySigner() public {
-    uint result = notaryStore.findAll(this);
-    // TODO: add test to listen for event
-    Assert.equal(result, 2, "should find 1 result");
-
-    notaryStore.create(anotherHash);
-    notaryStore.create(foobarHash);
-
-    uint results = notaryStore.findAll(this);
-    // TODO: add test to listen for events
-    Assert.equal(results, 4, "should find multiple hashes");
-  }
-
-  function testFindAllByHash() public {
-    uint result = notaryStore.findAll(anotherHash);
-    // TODO: add test to listen for event
-    Assert.equal(result, 2, "should find 1 signer");
+    Assert.isTrue(notaryStore.create(foobarHash), "creating an entry");
+    // TODO: write test that asserts an event was created.
   }
 
 }
